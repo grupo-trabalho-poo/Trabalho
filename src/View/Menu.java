358240c885a0a3e289be1ceeea568e98907f5dd9
jadson correa de almeida <jadson.correa@gmail.com>
2015-05-03 16:01:45 -0300

    Teste2

--- src/View/Menu.java
+++ src/View/Menu.java
@@ -6,7 +6,7 @@ import Model.Pojo.Aluno;
 public class Menu {
 
 	public static void main(String[] args) {
-		
+		System.out.println(" UFLN ");
 		AlunoDaoImp alunoDaoImp = new AlunoDaoImp();
 		AlunoView alunoView = new AlunoView(alunoDaoImp);
 		alunoView.cadastrar();
