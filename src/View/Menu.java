57d25e46122d61c76811fae3b5756ad921c30d58
jadson correa de almeida <jadson.correa@gmail.com>
2015-05-11 17:06:04 -0300

    corrigindo main

--- src/View/Menu.java
+++ src/View/Menu.java
@@ -2,48 +2,49 @@ package View;
 
 import Model.Dao.AlunoDaoImp;
 import Model.Dao.AtividadeDaoImp;
+import Model.Dao.DisciplinaDaoImp;
 import Model.Dao.ProfessorDaoImp;
 
 public class Menu {
 
 	public static void main(String[] args) {
 		
-//		AlunoDaoImp alunoDaoImp = new AlunoDaoImp();
-//		AlunoView alunoView = new AlunoView(alunoDaoImp);
-//		alunoView.cadastrar();
-//		alunoView.cadastrar();
-//		alunoView.cadastrar();
-//		alunoView.listar();
-//		alunoView.removerAluno();
-//		alunoView.listar();
+		AlunoDaoImp alunoDaoImp = new AlunoDaoImp();
+		AlunoView alunoView = new AlunoView(alunoDaoImp);
+		alunoView.cadastrar();
+		alunoView.cadastrar();
+		alunoView.cadastrar();
+		alunoView.listar();
+		alunoView.removerAluno();
+		alunoView.listar();
 		
-//		ProfessorDaoImp professorDaoImp = new ProfessorDaoImp();
-//		ProfessorView professorView = new ProfessorView(professorDaoImp);
-//		professorView.cadastrar();
-//		professorView.cadastrar();
-//		professorView.listar();
-//		professorView.removerProfessor();
-//		professorView.listar();
-		
-		
-		
-//		AtividadeDaoImp atividadeDaoImp = new AtividadeDaoImp();
-//		AtividadeView atividadeView = new AtividadeView(atividadeDaoImp);
-//		atividadeView.cadastrar();
-//		atividadeView.cadastrar();
-//		atividadeView.cadastrar();
-//		atividadeView.listar();
-//		atividadeView.removerAtividade();
-//		atividadeView.listar();
+		ProfessorDaoImp professorDaoImp = new ProfessorDaoImp();
+		ProfessorView professorView = new ProfessorView(professorDaoImp);
+		professorView.cadastrar();
+		professorView.cadastrar();
+		professorView.listar();
+		professorView.removerProfessor();
+		professorView.listar();
 		
 		
-//		DisciplinaDaoImp disciplinaDaoImp = new DisciplinaDaoImp();
-//		DisciplinaView disciplinaView = new DisciplinaView(disciplinaDaoImp);
-//		disciplinaView.cadastrar();
-//		disciplinaView.cadastrar();
-//		disciplinaView.listar();
-//		disciplinaView.removerDisciplina();
-//		disciplinaView.listar();
+		
+		AtividadeDaoImp atividadeDaoImp = new AtividadeDaoImp();
+		AtividadeView atividadeView = new AtividadeView(atividadeDaoImp);
+		atividadeView.cadastrar();
+		atividadeView.cadastrar();
+		atividadeView.cadastrar();
+		atividadeView.listar();
+		atividadeView.removerAtividade();
+		atividadeView.listar();
+		
+		
+		DisciplinaDaoImp disciplinaDaoImp = new DisciplinaDaoImp();
+		DisciplinaView disciplinaView = new DisciplinaView(disciplinaDaoImp);
+		disciplinaView.cadastrar();
+		disciplinaView.cadastrar();
+		disciplinaView.listar();
+		disciplinaView.removerDisciplina();
+		disciplinaView.listar();
 		
 
 		
